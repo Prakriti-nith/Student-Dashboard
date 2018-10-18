79a124bf4e65605cb9a5f7844e16367e13823d87
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-10-19 02:38:40 +0530

    Added login and dashboard

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, flash, redirect, url_for, ses
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -14,10 +15,12 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # init MYSQL
 mysql = MySQL(app)
 
+# Index
 @app.route('/')
 def index():
     return render_template('home.html')
 
+# Register form class
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
@@ -28,6 +31,7 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
+# USer Register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -54,6 +58,68 @@ def register():
         return redirect(url_for('index'))
 
     return render_template('register.html', form=form)
+
+# User login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        roll_number = request.form['roll_number']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by roll_number
+        result = cur.execute("SELECT * FROM user WHERE roll_number = %s", [roll_number])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password'] 
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # passed
+                app.logger.info('PASSWORD MATCHED')
+                session['logged_in'] = True
+                session['roll_number'] = roll_number
+
+                flash('Yoe are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid Login'
+                return render_template('login.html', error=error)
+            cur.close
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+
+    return render_template('login.html')
+
+# Decorator: Checks if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.secret_key='secret123'
